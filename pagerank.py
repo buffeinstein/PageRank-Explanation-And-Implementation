ef1acb0071b2911c6b4b6cb1a98d79ee9efecde7
Ambika Tiwari <ambikatiwari999@gmail.com>
2024-09-22 16:18:03 -0700

    task 2 done?

--- pagerank.py
+++ pagerank.py
@@ -104,10 +104,11 @@ class WebGraph():
 
         else:
             v = torch.zeros(n)
-            # FIXME: implement Task 2
-        
-        v_sum = torch.sum(v)
-        assert(v_sum>0)
+            for i in range(0, n):
+                url = self._index_to_url(i)
+                if url_satisfies_query(url, query):
+                    v[i] = 1
+        assert(torch.sum(v)>0)
         v /= v_sum
 
         return v
